0da7333d38b04210a1c220b16abe1584a4930dee
xycarto <ian@xycarto.com>
2024-01-15 17:31:36 +1300

    better function on nav bar

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -7,3 +7,15 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("navbutton").style.marginLeft= "0";
 }
+
+function toggleNav() {
+  const element = document.getElementById("mySidebar");
+  const navb = document.getElementById("navbutton");
+  if (element.style.width == "500px" && navb.style.marginLeft == "500px") {
+      element.style.width = "0px";
+      navb.style.marginLeft= "0";
+  } else {
+      element.style.width = "500px";
+      navb.style.marginLeft = "500px";
+  }
+}
